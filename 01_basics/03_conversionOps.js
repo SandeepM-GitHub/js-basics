362c2891fa5631b05e76bfdbbb91225c819229c4
SandeepM-GitHub <142106485+SandeepM-GitHub@users.noreply.github.com>
2024-08-09 07:33:11 +0000

    conversionOps done

--- 01_basics/03_conversionOps.js
+++ 01_basics/03_conversionOps.js
@@ -1,4 +1,4 @@
-let score = "33abc"
+// let score = "33abc"
 
 // console.log(typeof score);
 // console.log(typeof(score)); // method type declaration of "typeof"
@@ -11,12 +11,27 @@ let score = "33abc"
 // "33abc" > NaN
 // true > 1, false > 0;
 
-let isLoggedIn = "Sandy"
+// let isLoggedIn = "Sandy"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 // 1 > true, 0 > false
 // "" > false
 // "sandy" > true
+
+// let str1 = "hello"
+// let str2 = " Sandy"
+
+// let str3 = str1 + str2;
+// console.log(str3); // hello Sandy
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+console.log(true); // true
+console.log(+true); // 1
+console.log(+""); // 0
 
