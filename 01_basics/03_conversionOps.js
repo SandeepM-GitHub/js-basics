a8a83dce3f944a5a03c743d053f02639c6de4696
SandeepM-GitHub <142106485+SandeepM-GitHub@users.noreply.github.com>
2024-08-09 06:04:36 +0000

    conversionOps Data type

---
+++ 01_basics/03_conversionOps.js
@@ -0,0 +1,22 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score)); // method type declaration of "typeof"
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" > 33
+// "33abc" > NaN
+// true > 1, false > 0;
+
+let isLoggedIn = "Sandy"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 > true, 0 > false
+// "" > false
+// "sandy" > true
+
