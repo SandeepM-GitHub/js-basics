256cbdea4a5d56c3c654b17c22f6946365c110a1
SandeepM-GitHub <142106485+SandeepM-GitHub@users.noreply.github.com>
2024-08-11 00:27:12 +0000

    data Types done

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -6,8 +6,31 @@ Reference (Non-Primitive)
 - Array, Objects, Functions, 
 */
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
-console.log(id === anotherId); // false, because its the characteristics of the 'Symbol' type
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id === anotherId); // false, because its the characteristics of the 'Symbol' type
 
 // Data types need to be learnt from ecmaScript.
+
+// Stack(Primitive), Heap(Non-Primitive).
+
+let myName = "Sandeep"
+let anotherName = myName
+console.log(anotherName);
+
+anotherName = "Sandy"
+
+console.log(myName);
+console.log(anotherName);
+
+let userOne = {
+    email: "userOne@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne;
+console.log(userTwo.email);
+
+userTwo.email = "Jacob@gmail.com"
+
+console.log(userTwo.email);
