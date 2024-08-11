3c81bece0c776639642018fd77e8ff9560ef6d5e
SandeepM-GitHub <142106485+SandeepM-GitHub@users.noreply.github.com>
2024-08-11 11:25:36 +0000

    ARRAY 1

---
+++ 02_basics/arrays.js
@@ -0,0 +1,18 @@
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeroes = ["Shaktiman", "Bhaktiman"]
+
+const myArr2 = new Array(1, 2, 3, 4, 5, 6)
+// console.log(myArr);
+// console.log(myArr2);
+
+// Array methods
+
+myArr2.push(7)
+console.log(myArr2);
+myArr2.pop(6)
+console.log(myArr2);
+
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(3));
+
+
